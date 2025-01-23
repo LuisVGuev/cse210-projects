7e534c166f9f9f046be2d37b1ae0ff66d198f77a
LuisVGuev <tag.interaction.web@gmail.com>
2025-01-22 23:10:10 -0300

    main

---
+++ Activity/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Job job1 = new Job("Software Engineer", "Microsoft", 2019, 2022);
+        Job job2 = new Job("Manager", "Apple", 2022, 2023);
+
+        Resume myResume = new Resume("Allison Rose");
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+
+        myResume.Display();
+    }
+}
